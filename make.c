99de2ba378873e8b60b84824e7d539224bd17020
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-02-18 22:48:50 +0100

    Updates

--- make.c
+++ make.c
@@ -2,16 +2,30 @@
 # include <stdlib.h>
 # include <string.h>
 
+
+/**
+ * main - The main function
+ * Description - This program is to make compilation of c code easier by reducing the compiler commands
+ * @argc: Number of command line arguments
+ * @argv: List of command Line arguments
+ * 
+ * Return 0: success
+*/
 int main(int argc, char *argv[])
 {
+     /* Initialize Variable to store derived command */
      char command[100];
 
+     /* If command line arguments is 3 */
      if (argc == 3) {
 
+          /* if correct option "-f" is passed in to compile folder */
           if (strcmp(argv[1], "-f") == 0){
 
+               /* Generate and store command*/
                sprintf(command, "gcc *.c -o %s -Wall -Werror -pedantic -Wextra -std=gnu89", argv[2]);
 
+               /* run command */
                system(command);
           }
 
@@ -22,10 +36,13 @@ int main(int argc, char *argv[])
           }
      }
 
+     /* If command line arguments is 2 */
      else if (argc == 2) {
 
+          /* Generate and store command*/
           sprintf(command, "gcc %s.c -o %s -Wall -Werror -pedantic -Wextra -std=gnu89", argv[1], argv[1]);
 
+          /* run command */
           system(command);
 
      }
